a5036c9668c5e8b3be3ba1bf140ae626bac4eb88
waleZeraf <34514614+waledagne@users.noreply.github.com>
2022-01-31 11:36:00 -0800

    style: navbars

--- components/nav.vue
+++ components/nav.vue
@@ -1,14 +1,16 @@
 <template>
-  <div class="shadow-lg">
-    <div class="flex justify-start">
-      <img src="~/assets/images/logo1.png" alt="" class="w-16 h-16 mt-1" />
+  <div class="w-1/2 md:w-full shadow-lg">
+    <div class="flex justify-start px-4 mt-4">
+      <img src="~/assets/images/logo1.png" alt="" class="w-10 h-10 mt-4" />
     </div>
     <div class="justify-end items-end flex flex-row">
-      <ul class="text-white flex flex-row justify-between mx-2">
-        <li class="mx-2">about</li>
-        <li class="mx-2">works</li>
-        <li class="mx-2">contact</li>
-        <li><button class="btn btn-sm btn-outline-accent">resume</button></li>
+      <ul class="text-white flex flex-row justify-between mx-2 mb-8">
+        <li class="mx-2 hover:text-accent">about</li>
+        <li class="mx-2 hover:text-accent">works</li>
+        <li class="mx-4 hover:text-accent">contact</li>
+        <li>
+          <button class="btn btn-xs btn-outline btn-accent">resume</button>
+        </li>
       </ul>
     </div>
   </div>
