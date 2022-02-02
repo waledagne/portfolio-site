ad4ee5fbb8568e3835e1de48d05554854b43e9a4
waleZeraf <34514614+waledagne@users.noreply.github.com>
2022-02-02 03:59:27 -0800

    style:global css scroll bar added

--- components/about.vue
+++ components/about.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="about">
-    <h1 class="text-white">hello there</h1>
-    <p class="text-white">
+    <h1 class="text-gray-300 mx-8">hello there</h1>
+    <p class="text-gray-300 mx-8">
       Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia ipsam
       laboriosam quas impedit, at voluptatibus eveniet neque tenetur,
       reprehenderit pariatur reiciendis earum? Exercitationem, iure! Quidem quia
--- components/nav.vue
+++ components/nav.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="w-1/2 md:w-full shadow-lg">
+  <div class="w-full shadow-lg">
     <div class="flex justify-start py-4 mx-4">
       <img src="~/assets/images/logo2.png" alt="" class="w-10 h-10" />
     </div>
@@ -20,9 +20,65 @@
         </li>
       </ul>
     </div>
+    <div v-if="sidenavOn" class="side-nav">
+      <div>
+        <a @click="sidenavOn = false" href="#home" class="nav-item">Home</a>
+
+        <a @click="sidenavOn = false" href="#about" class="nav-item">About</a>
+
+        <a @click="sidenavOn = false" href="#skills" class="nav-item">Skills</a>
+
+        <a @click="sidenavOn = false" href="#works" class="nav-item">Works</a>
+
+        <a @click="sidenavOn = false" href="#contact" class="nav-item"
+          >Contact</a
+        >
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  return: {
+    sidenavOn: false,
+  },
+};
 </script>
+
+<style>
+.side-nav {
+  width: 100vw;
+  height: 100vh;
+  background-image: linear-gradient(
+    to bottom left,
+    rgba(0, 0, 0, 0.775),
+    #2c3e50
+  );
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 9;
+  animation: slidedown 0.45s;
+}
+@keyframes slidein {
+  from {
+    left: -200px;
+  }
+  to {
+    left: 0;
+  }
+}
+@keyframes slidedown {
+  from {
+    top: -1000px;
+  }
+  to {
+    top: 0;
+  }
+}
+</style>
