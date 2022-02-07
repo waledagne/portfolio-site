a6927cc2d2b355552838e9bb64700f4bde6acfb8
waleZeraf <34514614+waledagne@users.noreply.github.com>
2022-02-07 08:07:17 -0800

    theme color configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,33 +1,35 @@
 const { secondary } = require("daisyui/colors/colorNames");
 
 module.exports = {
-  purge: ["./components/**/*.{js,vue,ts}",
-  "./layouts/**/*.vue",
-  "./pages/**/*.vue",
-  "./plugins/**/*.{js,ts}",
-  "./nuxt.config.{js,ts}",],
+  purge: [
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./nuxt.config.{js,ts}",
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
-      colors:{
-        black:"#0a192f"
+      colors: {
+        black: "#0a192f",
       },
-      mytheme:{
-        primary: '#2a9d8f',
-        'primary-focus': '#264653',
-        'primary-content': '#ffffff',
-        secondary: '#F2B200',
-        'secondary-focus': '#c28e00',
-        'secondary-content': '#ffffff',
-        accent: '#64ffda',
-        'accent-focus': '#005542',
-        'accent-content': '#ffffff',
 
-      }
+      mytheme: {
+        primary: "#2a9d8f",
+        "primary-focus": "#264653",
+        "primary-content": "#ffffff",
+        secondary: "#F2B200",
+        "secondary-focus": "#c28e00",
+        "secondary-content": "#ffffff",
+        accent: "#64ffda",
+        "accent-focus": "#005542",
+        "accent-content": "#ffffff",
+      },
     },
   },
   variants: {
     extend: {},
   },
-  plugins:[require('daisyui')],
-}
+  plugins: [require("daisyui")],
+};
