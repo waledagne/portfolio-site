b9f4edaf4732b4d01b1fea74cfdcf860be7922f2
waleZeraf <34514614+waledagne@users.noreply.github.com>
2022-02-09 01:09:33 -0800

    favicon update

--- components/about.vue
+++ components/about.vue
@@ -12,6 +12,12 @@
             tenetur, reprehenderit pariatur reiciendis earum? Exercitationem,
             iure! Quidem quia placeat optio quos facere?
           </p>
+          <p class="text-gray-400">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia
+            ipsam laboriosam quas impedit, at voluptatibus eveniet neque
+            tenetur, reprehenderit pariatur reiciendis earum? Exercitationem,
+            iure! Quidem quia placeat optio quos facere?
+          </p>
         </div>
         <div class="mb-6">
           <img
--- pages/index.vue
+++ pages/index.vue
@@ -7,7 +7,9 @@
       </div>
 
       <div class="flex flex-row"></div>
-      <div class="flex flex-col justify-start items-start px-20 mx-24 mt-10">
+      <div
+        class="flex flex-col justify-start items-start px-20 mx-24 mt-10 mb-32"
+      >
         <h1 class="text-accent text-2xl">Hi, my name is</h1>
         <h1 class="text-gray-200 font-semibold text-6xl mt-4">
           Walelgn Dagne.
@@ -15,7 +17,7 @@
         <h1 class="text-gray-500 text-6xl font-semibold mt-4">
           I build things for the web
         </h1>
-        <p class="w-full md:w-2/3 text-gray-400 text-lg mt-4">
+        <p class="w-full md:w-2/3 text-gray-400 text-lg mt-4 mb-32">
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore,
           repellat nostrum repellendus vero accusantium magnam blanditiis
           corporis neque nobis assumenda autem suscipit officia qui debitis.
