83f07f06557b55ad9bc116a0ffeabbec7090e7c3
waleZeraf <34514614+waledagne@users.noreply.github.com>
2022-02-11 08:45:15 -0800

    removed uncessary code

--- components/nav.vue
+++ components/nav.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="w-full shadow-lg">
+  <div class="w-full md:shadow-lg">
     <div class="flex justify-start md:py-4 mx-4">
-      <img src="~/assets/images/logo2.png" alt="" class="w-10 h-10" />
+      <img src="~/assets/images/logo2.png" alt="" class="sm:hidden w-10 h-10" />
     </div>
     <div class="desktop-nav justify-end items-end flex flex-row">
       <ul class="text-white flex flex-row justify-between mx-2">
@@ -21,52 +21,6 @@
       </ul>
     </div>
 
-    <!-- <nav class="mobile-nav">
-      <div class="">
-        <div
-          class="text-white"
-          v-if="!sidenavActive"
-          @click="sidenavActive = true"
-        >
-          <i class="fa fa-bars text-accent text-3xl"></i>
-        </div>
-        <div v-else @click="sidenavActive = false">
-          <i class="fa fa-times text-accent text-3xl"></i>
-        </div>
-      </div>
-    </nav>
-
-    <div v-if="sidenavActive" class="sidenav">
-      <div
-        class="w-full flex flex-col justify-start items-center border-b-2 mb-5 pb-2"
-      >
-        <div class="text-center">
-          <div
-            class="mx-auto w-14 h-14 overflow-hidden rounded-full border-2 border-accent shadow-lg"
-          ></div>
-          <div class="w-full flex items-center justify-between"></div>
-        </div>
-        <nuxt-link
-          class="text-lg mb-2"
-          to="/"
-          @click.native="sidenavActive = false"
-          >Home</nuxt-link
-        >
-        <nuxt-link
-          class="text-lg mb-2"
-          to="/about"
-          @click.native="sidenavActive = false"
-          >About</nuxt-link
-        >
-
-        <nuxt-link class="text-lg mb-2" to="/gaiaverse">Gaiaverse</nuxt-link>
-      </div>
-      <div
-        v-if="sidenavActive"
-        class="bg-black bg-opacity-50 fixed top-0 left-0 z-40 w-screen h-screen"
-        @click="sidenavActive = false"
-      ></div>
-    </div> -->
     <nav class="mobile-nav">
       <nuxt-link to="/" class="flex items-center text-xl font-bold text-accent">
         <img src="~/assets/images/logo2.png" class="w-10" alt="" />
@@ -142,7 +96,7 @@ export default {
 
 <style>
 nav {
-  @apply w-full  px-16 flex justify-between items-center h-14 shadow-md  fixed top-0 left-0 z-50;
+  @apply w-full  px-16 flex justify-between items-center h-14 shadow-md  fixed top-0 left-0 z-50 md:hidden;
 }
 .desktop-nav {
   @apply hidden sm:flex;
@@ -153,7 +107,7 @@ nav {
 }
 
 .sidenav {
-  @apply w-3/5 md:hidden py-5 px-6 fixed rounded-lg top-14 right-0 bg-white shadow text-gray-800 z-50 flex flex-col justify-start items-start;
+  @apply w-full md:hidden py-5 px-6 fixed rounded-lg top-14 right-0 bg-white shadow text-gray-800 z-50 flex flex-col justify-start items-start;
   animation: slidein 0.4s forwards;
 }
 
