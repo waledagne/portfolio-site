a8cb01fd9f4adf67558e04303c52943a6eaad056
waleZeraf <34514614+waledagne@users.noreply.github.com>
2022-02-11 04:12:09 -0800

    make sidenav works

--- components/nav.vue
+++ components/nav.vue
@@ -21,15 +21,14 @@
       </ul>
     </div>
 
-    <nav class="mobile-nav">
-      <div class="inline-flex relative">
+    <!-- <nav class="mobile-nav">
+      <div class="">
         <div
           class="text-white"
           v-if="!sidenavActive"
           @click="sidenavActive = true"
         >
           <i class="fa fa-bars text-accent text-3xl"></i>
-          mo
         </div>
         <div v-else @click="sidenavActive = false">
           <i class="fa fa-times text-accent text-3xl"></i>
@@ -67,47 +66,98 @@
         class="bg-black bg-opacity-50 fixed top-0 left-0 z-40 w-screen h-screen"
         @click="sidenavActive = false"
       ></div>
+    </div> -->
+    <nav class="mobile-nav">
+      <div v-if="!sidenavActive" @click="sidenavActive = true" class="">
+        <i class="fa fa-bars text-accent text-3xl"></i>
+      </div>
+      <div v-else @click="sidenavActive = false">
+        <i class="fa fa-times text-accent text-3xl"></i>
+      </div>
+    </nav>
+
+    <div v-if="sidenavActive" class="sidenav">
+      <nuxt-link
+        class="text-lg mb-2"
+        to="/"
+        @click.native="sidenavActive = false"
+        >Home</nuxt-link
+      >
+
+      <nuxt-link
+        class="text-lg mb-2"
+        to="/intuition"
+        @click.native="sidenavActive = false"
+        >Intuition</nuxt-link
+      >
+      <!-- <nuxt-link class="text-lg mb-2 ml-5" to="">About Us</nuxt-link> -->
+      <nuxt-link
+        class="text-lg mb-2"
+        to="/gaiaverse"
+        @click.native="sidenavActive = false"
+        >Gaia Verse</nuxt-link
+      >
+      <nuxt-link
+        class="text-lg mb-2"
+        to="/partners"
+        @click.native="sidenavActive = false"
+        >For Partners</nuxt-link
+      >
+      <nuxt-link
+        class="text-lg mb-2"
+        to="/contact"
+        @click.native="sidenavActive = false"
+        >Contact</nuxt-link
+      >
+      <div class="w-full flex items-center justify-between">
+        <button
+          class="btn btn-sm rounded-full border-accent bg-accent items-center"
+        >
+          <nuxt-link class="text-white mb-2" to="/signup">Signup</nuxt-link>
+        </button>
+        <nuxt-link to="/login" class="btn btn-link text-gray-600 btn-sm ml-5"
+          >Login</nuxt-link
+        >
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  return: {
-    sidenavActive: false,
+  data() {
+    return {
+      sidenavActive: false,
+    };
   },
 };
 </script>
 
 <style>
+/* nav {
+  @apply w-full  px-16 flex justify-between items-center h-14 shadow-md bg-white fixed top-0 left-0 z-50;
+} */
 .desktop-nav {
   @apply hidden sm:flex;
 }
 
 .mobile-nav {
-  @apply w-full md:hidden px-5 box-border justify-end items-end;
+  @apply sm:hidden  px-5;
 }
+
 .sidenav {
-  @apply w-3/5 md:hidden fixed rounded-lg top-14 right-0 bg-white shadow text-gray-800 z-50 flex flex-col justify-start items-start p-5;
+  @apply w-3/5 md:hidden py-5 px-6 fixed rounded-lg top-14 right-0 bg-white shadow text-gray-800 z-50 flex flex-col justify-start items-start;
   animation: slidein 0.4s forwards;
 }
-.animate-slide-down {
-  animation: slidein 0.4s forwards;
-}
+
 @keyframes slidein {
-  from {
-    left: -200px;
+  0% {
+    transform: translateY(-100px);
+    opacity: 0;
   }
-  to {
-    left: 0;
-  }
-}
-@keyframes slidedown {
-  from {
-    top: -1000px;
-  }
-  to {
-    top: 0;
+  100% {
+    transform: translateY(0px);
+    opacity: 1;
   }
 }
 </style>
