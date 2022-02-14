894273bf98f6e8c873cceae781317213bea769eb
waleZeraf <34514614+waledagne@users.noreply.github.com>
2022-02-14 08:56:09 -0800

    style:works page

--- components/works.vue
+++ components/works.vue
@@ -1,46 +1,85 @@
 <template>
   <div id="works">
     <div
-      class="w-full text-gray-300 text-2xl md:text-3xl font-semibold flex flex-col md:mx-24 md:px-20 mt-40 justify-start"
+      class="w-full text-gray-300 text-2xl md:text-3xl font-semibold flex flex-col md:mx-24 md:px-20 mt-44 justify-start"
     >
       <h2>A few things I built</h2>
       <!-- make image cards  -->
-      <div
-        class="flex flex-col md:flex-row md:justify-start md:items-start mt-8 relative"
-      >
-        <div class="w-full md:mx-4 relative">
-          <div class="rounded-lg shadow-lg p-4">
-            <div class="flex flex-row justify-center items-center">
-              <img
-                src="~/assets/images/alemekina.png"
-                alt=""
-                class="w-full h-96"
-              />
+      <div class="flex flex-col">
+        <div
+          class="flex flex-col md:flex-row md:justify-start md:items-start mt-8 relative"
+        >
+          <div class="w-full md:mx-4 relative">
+            <div class="rounded-lg shadow-lg p-4">
+              <div class="flex flex-row justify-center items-center">
+                <img
+                  src="~/assets/images/alemekina.png"
+                  alt=""
+                  class="w-full h-96"
+                />
+              </div>
+            </div>
+          </div>
+          <div class="flex flex-col">
+            <div
+              class="shadow-lg rounded-sm w-full md:w-2/3 bg-blueblack mr-20 mx-0 mt-10 z-10 relative"
+            >
+              <p class="text-sm text-gray-400 px-8">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae
+                error explicabo exercitationem tenetur, dignissimos, at deleniti
+                sit fugiat placeat temporibus officia dolor minus dolorum nisi
+                inventore ipsa nesciunt itaque. Fuga!
+              </p>
+            </div>
+            <div class="flex flex-row mx-12">
+              <span class="font-thin text-gray-400 text-lg mt-2 mx-2">Vue</span>
+              <span class="font-thin text-gray-400 text-lg mt-2 mx-2"
+                >Nuxt Js</span
+              >
+              <span class="font-thin text-gray-400 text-lg mt-2 mx-2"
+                >Firebase</span
+              >
+              <span class="font-thin text-gray-400 text-lg mt-2"
+                >Tailwind Css</span
+              >
             </div>
           </div>
         </div>
-        <div class="flex flex-col">
-          <div
-            class="shadow-lg rounded-sm w-2/3 bg-blueblack mr-20 mx-0 mt-10 z-10 relative"
-          >
-            <p class="text-sm text-gray-400 px-8">
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae
-              error explicabo exercitationem tenetur, dignissimos, at deleniti
-              sit fugiat placeat temporibus officia dolor minus dolorum nisi
-              inventore ipsa nesciunt itaque. Fuga!
-            </p>
+        <div class="flex flex-row">
+          <div class="flex flex-col">
+            <div
+              class="shadow-lg rounded-sm w-full md:w-2/3 bg-blueblack mr-20 mx-0 mt-10 z-10 relative"
+            >
+              <p class="text-sm text-gray-400 px-8">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae
+                error explicabo exercitationem tenetur, dignissimos, at deleniti
+                sit fugiat placeat temporibus officia dolor minus dolorum nisi
+                inventore ipsa nesciunt itaque. Fuga!
+              </p>
+            </div>
+            <div class="flex flex-row mx-12">
+              <span class="font-thin text-gray-400 text-lg mt-2 mx-2">Vue</span>
+              <span class="font-thin text-gray-400 text-lg mt-2 mx-2"
+                >Nuxt Js</span
+              >
+              <span class="font-thin text-gray-400 text-lg mt-2 mx-2"
+                >Firebase</span
+              >
+              <span class="font-thin text-gray-400 text-lg mt-2"
+                >Tailwind Css</span
+              >
+            </div>
           </div>
-          <div class="flex flex-row mx-12">
-            <span class="font-thin text-gray-400 text-lg mt-2 mx-2">Vue</span>
-            <span class="font-thin text-gray-400 text-lg mt-2 mx-2"
-              >Nuxt Js</span
-            >
-            <span class="font-thin text-gray-400 text-lg mt-2 mx-2"
-              >Firebase</span
-            >
-            <span class="font-thin text-gray-400 text-lg mt-2"
-              >Tailwind Css</span
-            >
+          <div class="w-full md:mx-4 relative">
+            <div class="rounded-lg shadow-lg p-4">
+              <div class="flex flex-row justify-center items-center">
+                <img
+                  src="~/assets/images/alemekina.png"
+                  alt=""
+                  class="w-full h-96"
+                />
+              </div>
+            </div>
           </div>
         </div>
       </div>
