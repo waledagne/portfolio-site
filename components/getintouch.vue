745aa03d83286db2e81aa6241c7a7195fa4e35d0
waleZeraf <34514614+waledagne@users.noreply.github.com>
2022-03-24 09:21:38 -0700

    get in touch responsive

--- components/getintouch.vue
+++ components/getintouch.vue
@@ -1,17 +1,21 @@
 <template>
-  <div class="flex flex-col mt-44 justify-center items-center mx-24 px-24">
-    <div class="w-1/2 justify-center items-center">
-      <p class="text-accent text-2xl px-36">What's next</p>
-      <h2 class="text-6xl text-gray-400 font-semibold mt-4 px-12">
+  <div
+    class="flex flex-col mt-44 justify-center items-center md:mx-24 md:px-24"
+  >
+    <div class="w-2/3 md:w-1/2 justify-center items-center">
+      <p class="text-accent text-2xl md:px-36">What's next</p>
+      <h2
+        class="text-3xl md:text-6xl text-gray-400 font-semibold mt-4 md:px-12"
+      >
         Get In Touch
       </h2>
-      <p class="text-gray-400 text-xl mt-8">
+      <p class="text-gray-400 md:text-xl mt-8">
         My inbox is always open. Whether you have a question or just want to say
         hi, I’ll try my best to get back to you!
       </p>
       <a href="" class="mt-4">
         <button
-          class="btn btn-lg btn-outline btn-accent hover:translate-x-11 mx-32 mt-6 mb-32"
+          class="btn btn-lg btn-outline btn-accent hover:translate-x-11 md:mx-32 mt-6 mb-32"
         >
           Contact Me
         </button>
