66cc8c4bb220c5d28e36faec1c66de2adc6cf3b3
waleZeraf <34514614+waledagne@users.noreply.github.com>
2022-03-25 09:25:08 -0700

    chore:skills component update

--- components/skills.vue
+++ components/skills.vue
@@ -1,26 +1,3 @@
 <template>
-  <div>
-    <!-- make a list of four span texts in flex col and  -->
-    <div class="flex flex-col justify-center items-center">
-      <span
-        class="text-lg hover:underline"
-        @click.native="sidenavActive = false"
-        >Home</span
-      >
-      <span
-        class="text-lg text-white mt-10"
-        @click.native="sidenavActive = false"
-        >About</span
-      >
-      <span class="text-lg mt-10" @click.native="sidenavActive = false"
-        >Works</span
-      >
-      <span class="text-lg mt-10" @click.native="sidenavActive = false"
-        >Contact</span
-      >
-      <span class="text-lg mt-10" @click.native="sidenavActive = false"
-        >Skills</span
-      >
-    </div>
-  </div>
+  <div></div>
 </template>
