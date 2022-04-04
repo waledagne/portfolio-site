93969a73e43b357f27fd81acf90b7a973e0941b5
waledagne <walelgndagne@gmail.com>
2022-04-04 15:36:30 +0300

    skill svg icons

--- components/skills.vue
+++ components/skills.vue
@@ -9,42 +9,58 @@
           <!-- list 4 items in flex col and list another 4 items in flex row -->
           <ul class="flex flex-col md:flex-row justify-start mt-8">
             <li class="flex flex-col w-full">
-              <h3 class="text-gray-300 text-lg font-semibold">
-                Front-End Development
-              </h3>
+              <div class="flex flex-row">
+                <h3 class="text-gray-300 text-lg font-semibold">HTML</h3>
+                <img class="px-2" src="~/assets/images/html.svg" alt="" />
+              </div>
+              <div class="flex flex-row">
+                <h3 class="text-gray-300 text-lg font-semibold mt-3">CSS</h3>
+                <img class="mx-4" src="~/assets/images/css3.svg" alt="" />
+              </div>
+              <div class="flex flex-row">
+                <h3 class="text-gray-300 text-lg font-semibold mt-3">
+                  Javascript
+                </h3>
+                <img
+                  class="mx-4 my-4"
+                  src="~/assets/images/javascript.svg"
+                  alt=""
+                />
+              </div>
+            </li>
+            <li class="flex flex-col w-full">
+              <div class="flex flex-row">
+                <h3 class="text-gray-300 text-lg font-semibold">Vue Js</h3>
+                <img class="px-2" src="~/assets/images/vuejs.svg" alt="" />
+              </div>
+              <div class="flex flex-row">
+                <h3 class="text-gray-300 text-lg font-semibold mt-3">
+                  Nuxt Js
+                </h3>
+                <img class="mx-2" src="~/assets/images/nuxt.svg" alt="" />
+              </div>
               <h3 class="text-gray-300 text-lg font-semibold mt-3">
-                Front-End Development
-              </h3>
-              <h3 class="text-gray-300 text-lg font-semibold mt-3">
-                Front-End Development
+                Tailwind Css
               </h3>
             </li>
             <li class="flex flex-col w-full">
-              <h3 class="text-gray-300 text-lg font-semibold">
-                Back-End Development
-              </h3>
-              <h3 class="text-gray-300 text-lg font-semibold mt-3">
-                Back-End Development
-              </h3>
-              <h3 class="text-gray-300 text-lg font-semibold mt-3">
-                Back-End Development
-              </h3>
-            </li>
-            <li class="flex flex-col w-full">
+              <div class="flex flex-row">
+                <h3
+                  class="text-lg font-semibold text-transparent bg-clip-text bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500"
+                >
+                  Node Js
+                </h3>
+                <img class="mx-4" src="~/assets/images/node.svg" alt="" />
+              </div>
               <h3
-                class="text-lg font-semibold text-transparent bg-clip-text bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500"
+                class="text-lg font-semibold mt-3 text-transparent bg-clip-text bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500"
               >
-                Mobile Development
+                Laravel
               </h3>
               <h3
                 class="text-lg font-semibold mt-3 text-transparent bg-clip-text bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500"
               >
-                Mobile Development
-              </h3>
-              <h3
-                class="text-lg font-semibold mt-3 text-transparent bg-clip-text bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500"
-              >
-                Mobile Development
+                Mongo DB
               </h3>
             </li>
           </ul>
