09c9c36c3dcaa4ccf0dbacee1f0a381119b4d36c
waledagne <walelgndagne@gmail.com>
2022-04-08 11:34:52 +0300

    contact page

--- components/contact.vue
+++ components/contact.vue
@@ -1,3 +1,14 @@
 <template>
-  <div></div>
+  <div>
+    <Nav />
+  </div>
 </template>
+
+<script>
+import Nav from "../components/nav.vue";
+export default {
+  components: {
+    Nav,
+  },
+};
+</script>
